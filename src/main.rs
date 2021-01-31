7e934dbd67d30f4662e573a632103439255ab567
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-31 17:25:58 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
+use rand::prelude::*;
+
+fn display_random() {
+    let rand_int: u8 = random();
+    println!("{}", rand_int);
+
+    let coin_flip = random();
+    if coin_flip {
+        println!("Heads!");
+    } else {
+        println!("Tails!");
+    }
+
+    let mut rng = thread_rng();
+    let rand_float: f64 = rng.gen();
+    println!("x is: {}", rand_float);
+}
+
 fn greet() {
     println!("Hello, World!");
 }
 
 fn main() {
-    greet()
+    display_random();
+    greet();
 }
